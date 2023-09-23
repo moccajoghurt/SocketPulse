7b6d2cf11f74e0edaf688e9ea07d74c8ec2a5008
Marius Anderie <marius.anderie@nexplore.ch>
2023-09-23 19:11:09 +0200

    Feature/Add GetScreenBase64

---
+++ SocketPulse/SocketPulse.Receiver/Nodes/GetScreenBase64.cs
@@ -0,0 +1,12 @@
+﻿using SocketPulse.Receiver.Interfaces;
+using SocketPulse.Receiver.Service;
+
+namespace SocketPulse.Receiver.Nodes;
+
+public class GetScreenBase64 : IData
+{
+    public string Execute(Dictionary<string, string> arguments)
+    {
+        return SocketPulseReceiverSettings.GetScreenBase64();
+    }
+}
--- SocketPulse/SocketPulse.Receiver/Service/SocketPulseReceiverSettings.cs
+++ SocketPulse/SocketPulse.Receiver/Service/SocketPulseReceiverSettings.cs
@@ -3,6 +3,7 @@
 public static class SocketPulseReceiverSettings
 {
     public static uint TickRateMs { get; set; } = 100;
+    public static Func<string> GetScreenBase64 { get; set; } = () => "";
     internal static List<Func<string>> GetStateInfo { get; } = new();
     public static void RegisterGetStateFunction(Func<string> getStateInfo)
     {
