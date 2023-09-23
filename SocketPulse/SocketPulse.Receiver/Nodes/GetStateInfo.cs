ab1a790a21255064fb4de5121b18472d160eda46
Marius Anderie <marius.anderie@nexplore.ch>
2023-09-23 12:48:40 +0200

    Feature/Add GetStateInfo receiver node

---
+++ SocketPulse/SocketPulse.Receiver/Nodes/GetStateInfo.cs
@@ -0,0 +1,12 @@
+﻿using SocketPulse.Receiver.Interfaces;
+using SocketPulse.Receiver.Service;
+
+namespace SocketPulse.Receiver.Nodes;
+
+public class GetStateInfo : IData
+{
+    public string Execute(Dictionary<string, string> arguments)
+    {
+        return SocketPulseReceiverSettings.GetStateInfo.Aggregate("", (current, s) => current + s.Invoke());
+    }
+}
--- SocketPulse/SocketPulse.Receiver/Service/SocketPulseReceiverSettings.cs
+++ SocketPulse/SocketPulse.Receiver/Service/SocketPulseReceiverSettings.cs
@@ -3,4 +3,9 @@
 public static class SocketPulseReceiverSettings
 {
     public static uint TickRateMs { get; set; } = 100;
+    internal static List<Func<string>> GetStateInfo { get; } = new();
+    public static void RegisterGetStateFunction(Func<string> getStateInfo)
+    {
+        GetStateInfo.Add(getStateInfo);
+    }
 }
